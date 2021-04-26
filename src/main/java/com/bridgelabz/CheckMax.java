0b007a6d9f97f4fdd9136f2c133ce0f740601349
Pooja <pooja.14206@gmail.com>
2021-04-26 14:54:53 +0530

    added a file comtain for finding Integer max and its testcasees

--- src/main/java/com/bridgelabz/CheckMax.java
+++ src/main/java/com/bridgelabz/CheckMax.java
@@ -1,5 +1,15 @@
 package com.bridgelabz;
 
 public class CheckMax {
+    public Integer integerMax(Integer number1, Integer number2, Integer number3) {
+        Integer max = number1;
+        if (number2.compareTo(max) > 0) {
+            max = number2;
+        }if (number3.compareTo(max) > 0) {
+            max = number3;
+        }
+        return max;
+    }
+
 
 }
--- src/test/java/com/bridgelabz/ValidateMax.java
+++ src/test/java/com/bridgelabz/ValidateMax.java
@@ -5,6 +5,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ValidateMax {
+    CheckMax checkMax;
+
+    @Before
+    public void setUp() throws Exception {
+        checkMax = new CheckMax();
+    }
+
+    @Test
+    public void integerMaxAtFirstPositionTest() {
+        int valueToCheck = checkMax.integerMax(55,33,4);
+        Assert.assertEquals(55, valueToCheck);
+    }
+
+    @Test
+    public void integerMaxAtSecondPositionTest() {
+        int valueToCheck = checkMax.integerMax(22,33,4);
+        Assert.assertEquals(33, valueToCheck);
+    }
+
+    @Test
+    public void integerMaxAtThirdPositionTest() {
+        int valueToCheck = checkMax.integerMax(2,33,49);
+        Assert.assertEquals(49, valueToCheck);
+    }
 
 
 }
