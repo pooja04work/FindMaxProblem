0619bb9905de2a880bd88d15df40c9f27ca220ca
Pooja <pooja.14206@gmail.com>
2021-04-26 15:25:27 +0530

    added a file which check Max float value

--- src/main/java/com/bridgelabz/CheckMax.java
+++ src/main/java/com/bridgelabz/CheckMax.java
@@ -11,5 +11,15 @@ public class CheckMax {
         return max;
     }
 
+    public Float floatMax(Float number1, Float number2, Float number3) {
+        Float max = number1;
+        if (number2.compareTo(max) > 0) {
+            max = number2;
+        }if (number3.compareTo(max) > 0) {
+            max = number3;
+        }
+        return max;
+    }
+
 
 }
